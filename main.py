9d7815ef11d67fe0ee64f2bee58af01a3ab33dab
pierrevbc <saliou.p.ps@gmail.com>
2022-03-08 22:02:56 +0100

    first try

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
-
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
+from yahoo_data import *
+from logger import *
+import os
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+
+    log = Logger(os.getcwd())
+    get_ticker_info('msft')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
